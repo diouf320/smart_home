7dc49459feaaa8cc885d08b9b46a614a23d2214e
diouf320 <papaabdoudiouf7@gmail.com>
2024-10-28 11:08:05 +0100

    Hello Word

---
+++ src/main.c
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2012-2014 Wind River Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdio.h>
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
+#define LED_YELLOW_NODE DT_ALIAS(led_yellow)
+
+const struct gpio_dt_spec led_yellow_gpio = GPIO_DT_SPEC_GET_OR(LED_YELLOW_NODE, gpios, {0});
+
+int main(void)
+{
+	printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
+	gpio_pin_configure_dt(&led_yellow_gpio, GPIO_OUTPUT_HIGH);
+
+	return 0;
+}
