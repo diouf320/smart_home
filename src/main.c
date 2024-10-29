a1870b2843768bd525ca0c42393ceffce31a1891
diouf320 <papaabdoudiouf7@gmail.com>
2024-10-29 10:35:56 +0100

    Hello Word

--- src/main.c
+++ src/main.c
@@ -7,14 +7,19 @@
 #include <stdio.h>
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/i2c.h>
+
 #define LED_YELLOW_NODE DT_ALIAS(led_yellow)
+#define LCD_SCREEN_NODE DT_ALIAS(lcd_screen)
 
 const struct gpio_dt_spec led_yellow_gpio = GPIO_DT_SPEC_GET_OR(LED_YELLOW_NODE, gpios, {0});
+static const struct i2c_dt_spec lcd_screen = I2C_DT_SPEC_GET(LCD_SCREEN_NODE);
 
 int main(void)
 {
 	printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
 	gpio_pin_configure_dt(&led_yellow_gpio, GPIO_OUTPUT_HIGH);
+	//init_lcd(&lcd_screen);
 
 	return 0;
 }
